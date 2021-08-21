16114511ff02b5a63d00a492ac2cdefb28687bd6
AlanCarlosjp <alandepaulajr@gmail.com>
2021-08-20 21:47:02 -0300

    Created project

---
+++ BackEnd/TimeSheet-advogados/src/main/java/com/time/sheet/TimeSheetAdvogadosApplication.java
@@ -0,0 +1,13 @@
+package com.time.sheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSheetAdvogadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeSheetAdvogadosApplication.class, args);
+	}
+
+}
